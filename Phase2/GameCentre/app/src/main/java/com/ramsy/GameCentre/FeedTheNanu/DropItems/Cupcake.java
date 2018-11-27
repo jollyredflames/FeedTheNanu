41201d29ec22c3c2a363e5dace80142c9eb138ac
yingsi <elisachu21@gmail.com>
2018-11-26 21:11:37 -0500

    changing drop items image

---
+++ Phase2/GameCentre/app/src/main/java/com/ramsy/GameCentre/FeedTheNanu/DropItems/Cupcake.java
@@ -0,0 +1,36 @@
+package com.ramsy.GameCentre.FeedTheNanu.DropItems;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
+
+import com.ramsy.GameCentre.FeedTheNanu.Edible;
+import com.ramsy.slidingtiles.R;
+
+public class Cupcake extends android.support.v7.widget.AppCompatImageView implements Edible{
+
+    Bitmap image;
+
+    public Cupcake(Context context){
+        super(context);
+        this.image = BitmapFactory.decodeResource(getResources(), R.drawable.cupcake);
+        this.setImageBitmap(this.image);
+
+    }
+
+    @Override
+    public int effectOnLife() {
+        return 1;
+    }
+
+    @Override
+    public int effectOnScore() {
+        return 100;
+    }
+
+    @Override
+    public int effectOnSpeed() {
+        return 1;
+    }
+}
--- Phase2/GameCentre/app/src/main/java/com/ramsy/GameCentre/FeedTheNanu/DropItems/Spider.java
+++ Phase2/GameCentre/app/src/main/java/com/ramsy/GameCentre/FeedTheNanu/DropItems/Spider.java
@@ -1,27 +1,30 @@
 package com.ramsy.GameCentre.FeedTheNanu.DropItems;
+
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import com.ramsy.GameCentre.FeedTheNanu.Edible;
 import com.ramsy.slidingtiles.R;
 
-public class Melon extends android.support.v7.widget.AppCompatImageView implements Edible {
+public class Spider extends android.support.v7.widget.AppCompatImageView implements Edible {
 
-
-    Bitmap image = BitmapFactory.decodeResource(getResources(), R.drawable.melon);
-    public Melon(Context context){
+    Bitmap image = BitmapFactory.decodeResource(getResources(), R.drawable.spider);
+    public Spider(Context context){
         super(context);
         this.setImageBitmap(this.image);
+//        this.setMinimumHeight(150);
+//        this.setMaxHeight(150);
     }
+
 
     @Override
     public int effectOnLife() {
-        return 1;
+        return -1;
     }
 
     @Override
     public int effectOnScore() {
-        return 100;
+        return -200;
     }
 
     @Override
@@ -29,3 +32,4 @@ public class Melon extends android.support.v7.widget.AppCompatImageView implemen
         return 1;
     }
 }
+
