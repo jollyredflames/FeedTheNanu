223d31a7446aefbb1d1283b85c9b6087ca9932a8
yingsi <elisachu21@gmail.com>
2018-11-30 09:14:03 -0500

    WALKTHROUGH.pdf added

--- Phase2/GameCentre/app/src/main/java/com/ramsy/GameCentre/SlidingTiles/SlidingTileGameDelegate.java
+++ Phase2/GameCentre/app/src/main/java/com/ramsy/GameCentre/SlidingTiles/SlidingTileGameDelegate.java
@@ -1,5 +1,9 @@
 package com.ramsy.GameCentre.SlidingTiles;
 
+/**
+ *  An interface that the delegate of the sldingtiles can conform to,
+ *   in order receive information about events.
+ */
 public interface SlidingTileGameDelegate {
     void scoreDidChange(int newScore);
     void didComplete();
