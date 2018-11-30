df506c31b7d6e715621236a28963f707bd37c228
alexr <alexander.rabczak@mail.utoronto.ca>
2018-11-29 21:02:39 -0500

    added the factory to always return sliding tiles needs to be changed

--- Phase2/GameCentre/app/src/main/java/com/ramsy/GameCentre/GameCentreCommon/LeaderBoardFactory.java
+++ Phase2/GameCentre/app/src/main/java/com/ramsy/GameCentre/GameCentreCommon/LeaderBoardFactory.java
@@ -3,18 +3,19 @@ package com.ramsy.GameCentre.GameCentreCommon;
 public class LeaderBoardFactory {
 
     public LeaderBoardGeneralGame getGame(String gameName){
-        if(gameName == null){
-            return null;
-        }
-        if (gameName.equals("SlidingTiles")){
-            return new LeaderBoardSlidingTiles();
-        }
-        if (gameName.equals("FeedThenanu")){
-            return new LeaderBoardFeedTheNanu();
-        }
-        if (gameName.equals("MemoryMatrix")){
-            return new LeaderBoardMemoryMatrix();
-        }
-        return null;
+        return new LeaderBoardSlidingTiles();
+//        if(gameName == null){
+//            return null;
+//        }
+//        if (gameName.equals("SlidingTiles")){
+//            return new LeaderBoardSlidingTiles();
+//        }
+//        if (gameName.equals("FeedThenanu")){
+//            return new LeaderBoardFeedTheNanu();
+//        }
+//        if (gameName.equals("MemoryMatrix")){
+//            return new LeaderBoardMemoryMatrix();
+//        }
+//        return null;
     }
 }
